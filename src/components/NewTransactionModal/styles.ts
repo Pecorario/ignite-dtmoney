33e8b655286ce70e457bd985d30c58e028c592fe
Taynara Pecorario <taynara_rp22@hotmail.com>
2022-02-25 15:57:03 -0300

    finished modal and api

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,14 +1,19 @@
-import styled from "styled-components";
-import { darken } from 'polished';
+import styled from 'styled-components';
+import { darken, transparentize } from 'polished';
 
-export const  Container = styled.form`
-  h2 { 
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+export const Container = styled.form`
+  h2 {
     color: var(--text-title);
     font-size: 1.5rem;
     margin-bottom: 2rem;
   }
 
-  input { 
+  input {
     width: 100%;
     padding: 0 1.5rem;
     height: 4rem;
@@ -29,12 +34,12 @@ export const  Container = styled.form`
     }
   }
 
-  button[type="submit"] {
+  button[type='submit'] {
     width: 100%;
     padding: 0 1.5rem;
     height: 4rem;
     background: var(--green);
-    color: #FFF;
+    color: #fff;
     border-radius: 0.25rem;
     border: 0;
     font-size: 1rem;
@@ -46,7 +51,6 @@ export const  Container = styled.form`
     &:hover {
       filter: brightness(0.9);
     }
-
   }
 `;
 
@@ -57,12 +61,20 @@ export const TransactionTypeContainer = styled.div`
   gap: 0.5rem;
 `;
 
-export const RadioBox = styled.button`
+const colors = {
+  red: '#e52e4d',
+  green: '#33cc95'
+};
+
+export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
-  
-  background: transparent;
+
+  background: ${props =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : 'transparent'};
 
   display: flex;
   align-items: center;
@@ -71,7 +83,10 @@ export const RadioBox = styled.button`
   transition: border-color 0.2s;
 
   &:hover {
-    border-color: ${darken(0.1, '#d7d7d7')}; //vai escurecer a cor da borda(#d7d7d7) em 10% (0.1) 
+    border-color: ${darken(
+      0.1,
+      '#d7d7d7'
+    )}; //vai escurecer a cor da borda(#d7d7d7) em 10% (0.1)
   }
 
   img {
--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   margin-top: 4rem;
@@ -7,7 +7,7 @@ export const Container = styled.div`
     width: 100%;
     border-spacing: 0 0.5rem;
 
-    th { 
+    th {
       color: var(--text-body);
       font-weight: 400;
       padding: 1rem 2rem;
@@ -15,7 +15,7 @@ export const Container = styled.div`
       line-height: 1.5rem;
     }
 
-    td { 
+    td {
       padding: 1rem 2rem;
       border: 0;
       background: var(--shape);
@@ -27,7 +27,7 @@ export const Container = styled.div`
       }
 
       &.deposit {
-        color: var(--gree);
+        color: var(--green);
       }
 
       &.withdraw {
